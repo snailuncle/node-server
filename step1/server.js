9bdff9bbc54027af706e84eab41938bca2367ede
snailuncle <1789500304@qq.com>
2018-06-27 12:14:34 +0800

    nodejs静态页面

--- step1/server.js
+++ step1/server.js
@@ -6,26 +6,26 @@ var url = require('url')
 
 function staticRoot(staticPath, req, res){
   console.log(staticPath)
-  
+
   console.log(req.url)
   var pathObj = url.parse(req.url, true)
   console.log(pathObj)
-  
-  
+
+
   if(pathObj.pathname === '/'){
     pathObj.pathname += 'index.html'
   }
-  
 
-  
+
+
 
   var filePath = path.join(staticPath, pathObj.pathname)
-  
+
   // var fileContent = fs.readFileSync(filePath,'binary')
   // res.write(fileContent, 'binary')
   // res.end()
-  
-  
+
+
   fs.readFile(filePath, 'binary', function(err, fileContent){
     if(err){
       console.log('404')
@@ -35,15 +35,16 @@ function staticRoot(staticPath, req, res){
       console.log('ok')
       res.writeHead(200, 'OK')
       res.write(fileContent, 'binary')
-      res.end()      
+      res.end()
     }
   })
-  
+
 
 }
 
 console.log(path.join(__dirname, 'static'))
 
+console.log("__dirname=",__dirname)
 var server = http.createServer(function(req, res){
   staticRoot(path.join(__dirname, 'static'), req, res)
 })
